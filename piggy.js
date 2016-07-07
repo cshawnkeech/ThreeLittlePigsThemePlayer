2cf5c926a9c0bf761cbf496d7922d65a9d6d80c5
cshawnkeech <cshawnkeech@gmail.com>
2016-07-06 22:33:07 -0600

    Added wolf & theme
    Added wolf & theme. adjusted placement for 2/3 screen

--- piggy.js
+++ piggy.js
@@ -1,22 +1,22 @@
+
+
 //Piggy constructor
-
-
 function Piggy() {
   this.red = 235;
   this.green = 181;
   this.blue = 227;
   this.alpha = 240;
-  this.xTranslate = width/2 ;
+  this.xTranslate = width/3 ;
   this.yTranslate = height/2;
-  this.headtilt = 0;
-  this.sizeFactor = 2;
+  this.rotateFactor = 0;
+  this.scaleFactor = 2;
 
   this.display = function() {
     push()
 
     translate(this.xTranslate, this.yTranslate);
-    scale(this.sizeFactor);
-    rotate(this.headtilt);
+    scale(this.scaleFactor);
+    rotate(this.rotateFactor);
 
     // helper theme circle
     fill(this.red, this.green, this.blue , this.alpha);
@@ -53,7 +53,113 @@ function Piggy() {
   }//end this.display
 
   this.headBob = function() {
-    this.headtilt = this.headtilt++;
+    this.rotateFactor = this.rotateFactor++;
   }// end this.headBob
 
+  this.playTheme = function() {
+    var mouseDistance = dist(mouseX, mouseY, this.xTranslate, this.yTranslate);
+    if (mouseDistance < 40 * this.scaleFactor) {
+      helpersTheme.togglePlay();
+    } else if (mouseDistance > 40 * this.scaleFactor && helpersTheme.isEnded() === false) {
+      helpersTheme.stop();
+    }
+
+  }//end this.playTheme
+
+  this.spinCheck = function() {
+    if (helpersTheme.isPaused() === false && percent > 0) {
+      this.rotateFactor += 0.025;
+    } else if (helpersTheme.isPaused() || percent === 0) {
+      if (this.rotateFactor % TWO_PI > 0) {
+        this.rotateFactor = this.rotateFactor % TWO_PI;
+        this.rotateFactor -= 0.025;
+      } else if (this.rotateFactor < 0) {
+        this.rotateFactor = 0;
+      }
+    }
+  } // end this.spinCheck
+
+
 } // end Piggy constructor
+
+//Wolf constructor
+function Wolf() {
+  this.red = 235;
+  this.green = 181;
+  this.blue = 227;
+  this.alpha = 200;
+  this.xTranslate = 2 * width/ 3;
+  this.yTranslate = height/2;
+  this.scaleFactor = 0.80;
+  this.rotateFactor = 0;
+
+  this.display = function() {
+
+    push();//begins wolf
+    translate(this.xTranslate, this.yTranslate);
+    scale(this.scaleFactor);
+    rotate(this.rotateFactor);
+
+    strokeWeight(2);
+
+    fill(176, 176, 176);
+    quad(-100, -80, 100, -80, 20, 80, -20, 80);
+
+    fill(230, 225, 225);
+    quad(-93, -75, 93, -75, 73, -36, -73, -37);
+    quad(-20, 80, 20, 80, 14, 92, -14, 92);
+
+    //muzzle
+    stroke(161, 152, 152);
+    fill(176, 176, 176, 200);
+    quad(-10, 0, 10, 0, 19, 80, -18, 80);
+
+    stroke(0, 0, 0);
+    fill(176, 176, 176);
+    angleMode(DEGREES);
+    arc(0, 20, 200, 200, 218, 323);
+    angleMode(RADIANS);
+
+    //eyes
+    fill(168, 151, 151, 100);
+    triangle(-50, -26, -10, -13, -21, 1);
+    push(); //begins second eye
+    scale(-1, 1);
+    triangle(-50, -26, -10, -13, -21, 1);
+    pop(); //ends second eye
+
+    pop(); //ends wolf
+  } //ends this.display
+
+
+    this.headBob = function() {
+      this.rotateFactor = this.rotateFactor++;
+    }// end this.headBob
+
+    this.playTheme = function() {
+      var mouseDistance = dist(mouseX, mouseY, this.xTranslate, this.yTranslate);
+      if (mouseDistance < 100 * this.scaleFactor) {
+        wolvesTheme.togglePlay();
+      } else if (mouseDistance > 100 * this.scaleFactor && wolvesTheme.isEnded() === false) {
+        wolvesTheme.stop();
+      }
+
+    }//end this.playTheme
+
+    this.spinCheck = function() {
+      if (wolvesTheme.isPaused() === false && wolvesPercent > 0) {
+        this.rotateFactor += 0.025;
+      } else if (wolvesTheme.isPaused() || wolvesPercent === 0) {
+        if (this.rotateFactor % TWO_PI > 0) {
+          this.rotateFactor = this.rotateFactor % TWO_PI;
+          this.rotateFactor -= 0.025;
+        } else if (this.rotateFactor < 0) {
+          this.rotateFactor = 0;
+        }
+      }
+    } // end this.spinCheck
+
+
+
+
+}//end wolf constructor
--- sketch.js
+++ sketch.js
@@ -1,64 +1,66 @@
+
+//use this for github page
 var helpersTheme = new buzz.sound( "https://cshawnkeech.github.io/ThreeLittlePigsThemePlayer/audioExamples/HelpersTheme", {
     formats: [ "ogg", "mp3", "m4a", "wav" ]
-});//adjusted for github site
+});
 
-//var helpersTheme = new buzz.sound("audioExamples/helpersTheme.wav");
+var wolvesTheme = new buzz.sound( "https://cshawnkeech.github.io/ThreeLittlePigsThemePlayer/audioExamples/WolvesTheme", {
+    formats: ["ogg", "mp3", "m4a", "wav" ]
+});
+
+// //use this for local atom tests
+//
+// var helpersTheme = new buzz.sound( "audioExamples/HelpersTheme", {
+//     formats: [ "ogg", "mp3", "m4a", "wav" ]
+// });
+//
+// var wolvesTheme = new buzz.sound( "audioExamples/WolvesTheme", {
+//     formats: ["ogg", "mp3", "m4a", "wav" ]
+// });
+
 var pig;
+var wolf1;
 var percent;
-
+var wolvesPercent;
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
 
-
   pig = new Piggy();
-  //pig.display();
+  wolf1 = new Wolf();
+
 } // end setup
 
 function draw() {
   percent = helpersTheme.getPercent();
+  wolvesPercent = wolvesTheme.getPercent();
   background(45);
 
-  //testing text
-  push();
-  fill(245);
-  text(helpersTheme.isPaused(), 30, 30);
-  text(percent, 30, 50);
-  text(pig.headtilt, 30, 70);
-  text(pig.headtilt % TWO_PI, 30, 90)
-  pop();
-  //end testing text
-
+  // //testing text
+  // push();
+  // fill(245);
+  // text(helpersTheme.isPaused(), 30, 30);
+  // text(percent, 30, 50);
+  // text(pig.rotateFactor, 30, 70);
+  // text(pig.rotateFactor % TWO_PI, 30, 90)
+  // pop();
+  // //end testing text
 
   pig.display();
-  if (helpersTheme.isPaused() === false && percent > 0) {
-    pig.headtilt += 0.025;
-  } else if (helpersTheme.isPaused() || percent === 0) {
-    if (pig.headtilt % TWO_PI > 0) {
-      pig.headtilt = pig.headtilt % TWO_PI;
-      pig.headtilt -= 0.025;
-    } else if (pig.headtilt < 0) {
-      pig.headtilt = 0;
-    }
+  pig.spinCheck();
 
-  }
+  wolf1.display();
+  wolf1.spinCheck();
 
 } // end draw
 
-
-
 function mousePressed() {
-  var mouseDistance = dist(mouseX, mouseY, width/2, height/2);
-  if (mouseDistance < 40 * pig.sizeFactor) {
-    helpersTheme.togglePlay();
-  } else if (mouseDistance > 40 * pig.sizeFactor && helpersTheme.isEnded() === false) {
-    helpersTheme.stop();
-  }
-
-}//end mousePressed
+  pig.playTheme();
+  wolf1.playTheme();
+} //end mousePressed
 
 
-
+//for reference, from buzz example
 /*
 var mySound = new buzz.sound( "/sounds/myfile", {
     formats: [ "ogg", "mp3", "aac" ]
